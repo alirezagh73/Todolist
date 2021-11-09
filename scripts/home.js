9a95aea4e190540d58dd30f06178d413e5cf84d8
alireza gharghabi <alirza.gharghabi1@gmail.com>
2021-11-10 01:29:30 +0330

    Todolist V.1

---
+++ scripts/home.js
@@ -0,0 +1,149 @@
+const myForm = document.forms.myform;
+const formInp = document.getElementById("form-inp");
+const descInp = document.getElementById("description");
+const deuDateInp = document.getElementById("dueDate");
+const url = "https://6180cdd88bfae60017adfc0d.mockapi.io/todos";
+const addTaskBtn = document.getElementById("addtaskbtn");
+const saveTaskBtn = document.getElementById("savetaskbtn");
+myForm.addEventListener("submit", postTask);
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+const spinner = document.getElementById("spinner");
+
+async function postTask(e) {
+    e.preventDefault();
+    spinner.classList.remove("d-none");
+    let date = new Date();
+    const addTaskInpValue = formInp.value;
+    const descInpValue = descInp.value;
+    const dueDateValue = deuDateInp.value;
+    if (addTaskInpValue.trim() === "" || descInpValue.trim() === "" || dueDateValue === "") {
+        toastThirdInit();
+        spinner.classList.add("d-none");
+    } else {
+        let taskObj = {
+            title: addTaskInpValue,
+            description: descInpValue,
+            createdAt: date,
+            updatedAt: date,
+            checked: false,
+            dueDate: dueDateValue
+        }
+
+        let postingTask = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(taskObj),
+            headers: {
+                "content-type": "application/json"
+            },
+        });
+        if (!postingTask.ok) {
+            spinner.classList.add("d-none");
+            toastSecondInit();
+        } else {
+            spinner.classList.add("d-none");
+            toastFirstInit();
+        }
+        formInp.value = "";
+        descInp.value = "";
+        deuDateInp.value = "";
+    }
+}
+
+if (params.id) {
+    async function editTask() {
+        let index = params.id;
+        let taskObj;
+        let response = await fetch(url);
+        if (response.ok) {
+            taskObj = await response.json();
+        } else {
+            console.log("We Have an Error");
+        }
+        taskObj.reverse();
+        formInp.value = taskObj[index].title;
+        descInp.value = taskObj[index].description;
+        deuDateInp.value = taskObj[index].dueDate;
+        formInp.focus();
+        addTaskBtn.style.display = "none";
+        saveTaskBtn.style.display = "inline-block"
+
+    }
+
+    editTask();
+}
+
+
+async function saveTask() {
+
+    let taskObj;
+    let response = await fetch(url);
+    if (response.ok) {
+        taskObj = await response.json();
+    } else {
+        console.log("We Have an Error");
+    }
+    taskObj.reverse();
+    const saveindexval = params.id;
+    spinner.classList.remove("d-none");
+    taskObj[saveindexval].title = formInp.value;
+    taskObj[saveindexval].description = descInp.value;
+    taskObj[saveindexval].dueDate = deuDateInp.value;
+    taskObj[saveindexval].updatedAt = new Date();
+    let myUrl = `https://6180cdd88bfae60017adfc0d.mockapi.io/todos/${taskObj[saveindexval].id}`
+    let putTask = await fetch(myUrl, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(taskObj[saveindexval]),
+    });
+
+    if (putTask.status === 200) {
+        setTimeout(() => {
+
+            let queryParams = new URLSearchParams(window.location.search);
+            queryParams.delete("id");
+            history.replaceState(null, null, "index.html?" + queryParams.toString());
+        }, 500);
+        spinner.classList.add("d-none");
+        formInp.value = "";
+        descInp.value = "";
+        deuDateInp.value = "";
+        addTaskBtn.style.display = "inline-block";
+        saveTaskBtn.style.display = "none";
+        toastFourthInit();
+    } else {
+        spinner.classList.add("d-none");
+        queries = new URLSearchParams(window.location.search);
+        queries.delete("id");
+        history.replaceState(null, null, "error.html?" + queries.toString())
+        location.assign(window.location.search)
+    }
+
+}
+
+function toastFirstInit() {
+    let myToast = document.querySelector(".toast.toast-first");
+    let toastAlert = new bootstrap.Toast(myToast);
+    toastAlert.show();
+}
+
+function toastSecondInit() {
+    let myToast = document.querySelector(".toast.toast-second");
+    let toastAlert = new bootstrap.Toast(myToast);
+    toastAlert.show();
+}
+
+function toastThirdInit() {
+    let myToast = document.querySelector(".toast.toast-third");
+    let toastAlert = new bootstrap.Toast(myToast);
+    toastAlert.show();
+}
+
+function toastFourthInit() {
+    let myToast = document.querySelector(".toast.toast-fourth");
+    let toastAlert = new bootstrap.Toast(myToast);
+    toastAlert.show();
+}
+
---
+++ scripts/todolist.js
@@ -0,0 +1,226 @@
+"use strict"
+const taskGroup = document.querySelectorAll(".taskGroup")
+const url = "https://6180cdd88bfae60017adfc0d.mockapi.io/todos"
+const deleteBtn = document.getElementById("saveDeleteIndex");
+const btnPaginationList = document.querySelectorAll(".paginationBtn")
+const spinner=document.getElementById("spinner");
+
+async function showTask(page, size = 10) {
+    spinner.classList.add("d-none");
+    let taskObj;
+    let response = await fetch(url);
+    if (response.ok) {
+        taskObj = await response.json();
+    } else {
+        console.log("We Have an Error");
+    }
+
+    taskObj.reverse();
+
+    let arrayList = [];
+    arrayList = taskObj.slice().splice(page * size, size);
+
+
+    let queryParams = new URLSearchParams(window.location.search);
+
+    queryParams.set("page", `${+page+1}`);
+
+    history.pushState(null, null, "?" + queryParams.toString());
+
+
+    taskGroup.forEach((item, index) => {
+        let html = ``;
+
+
+        // console.log(arrayList)
+        // console.log(item,index);
+
+        for (let i = 0; i < arrayList.length; i++) {
+            // console.log(arrayList[i])
+            // console.log(taskObj)
+            if (arrayList[i].isChecked) {
+                // console.log(item.description);
+                // console.log(arrayList[i].title)
+                html += `<div class="row task-item">
+            <li class="col-8 col-md -10 list-group-item border-0 bg-transparent my-1 ">
+                <input class="form-check-input me-3" type="checkbox" id="" checked onclick="saveCheckbox(${(page * 10) + i})"  value="">
+           <span>${arrayList[i].title}</span>  
+            </li>
+            <div class="col-4 col-md-2 edit text-end">
+                <i class="fas fa-pencil-alt text-end mx-3 pt-3 text-primary d-none" role='button' onclick=editTask(${(page * 10) + i})></i>
+                <i class="far fa-trash-alt d-none text-danger" role='button' data-bs-toggle="modal" onclick="showModal(${(page * 10) + i})" data-bs-target="#exampleModal${(page * 10) + i}"></i>
+                <div class="modal fade" id="exampleModal${(page * 10) + i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content text-start">
+            <div class="modal-header">
+                <h5 class="modal-title text-danger  " id="exampleModalLabel">DELETE Modal </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                  <p>Do You want to  <span class="text-danger">delete</span> <span class="text-primary fs-3">${arrayList[i].title}</span> item?</p>
+                  <p class="text-dark">Description: <span class="text-primary">${arrayList[i].description}</span></p>
+                  <span class="fs-6">Due Date: <span class="text-primary">${arrayList[i].dueDate}</span></span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal"  onclick="deleteTask()">Delete item</button>
+            </div>
+        </div>
+    </div>
+</div>
+            </div>
+        </div>`
+            } else {
+                // console.log(arrayList[i].title)
+                html += `<div class="row task-item">
+            <li class="col-8 col-md-10 list-group-item border-0 bg-transparent my-1 ">
+                <input class="form-check-input me-3" type="checkbox" id="" onclick="saveCheckbox(${(page * 10) + i})"  value="">
+           <span>${arrayList[i].title}</span>  
+            </li>
+            <div class="col-4 col-md-2 edit text-end">
+                <i class="fas fa-pencil-alt text-end mx-3 pt-3 text-primary d-none" role='button' onclick=editTask(${(page * 10) + i})></i>
+                <i class="far fa-trash-alt d-none text-danger" role='button' data-bs-toggle="modal" onclick="showModal(${(page * 10) + i})" data-bs-target="#exampleModal${(page * 10) + i}" ></i>
+            <div class="modal fade" id="exampleModal${(page * 10) + i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content text-start">
+            <div class="modal-header">
+                <h5 class="modal-title text-danger" id="exampleModalLabel">DELETE Modal</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                  <p>Do You want to  <span class="text-danger">delete</span> <span class="text-primary fs-3">${arrayList[i].title}</span> item?</p>
+                  <p class="text-dark">Description: <span class="text-primary">${arrayList[i].description}</span></p>
+                  <span class="fs-6">Due Date: <span class="text-primary">${arrayList[i].dueDate}</span></span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger"  data-bs-dismiss="modal"  onclick="deleteTask()">Delete item</button>
+            </div>
+            
+        </div>
+    </div>
+</div>
+               </div>
+           </div>`
+                // console.log(item.description)
+            }
+
+        }
+        item.innerHTML = html;
+
+    })
+    const btnPag = document.querySelectorAll('.pagination__btn');
+
+    btnPag.forEach((elem, i) => {
+        elem.addEventListener('click', () => {
+            showTask(i);
+            addClass(elem,btnPag);
+        });
+    });
+
+}
+
+function editTask(index) {
+    let queryParams = new URLSearchParams(window.location.search);
+    // console.log(window.location)
+    queryParams.delete("page");
+    queryParams.set("id", `${index}`);
+    console.log(window.location.href);
+
+  history.replaceState(null, null, "index.html?" + queryParams.toString());
+    location.assign(window.location.href);
+}
+
+async function saveCheckbox(index) {
+
+    let taskObj;
+
+    let response = await fetch(url);
+    if (response.ok) {
+        taskObj = await response.json();
+    } else {
+        console.log("We have an Error");
+    }
+    taskObj.reverse();
+    if (taskObj[index].isChecked) {
+        taskObj[index].isChecked = false;
+    } else {
+        taskObj[index].isChecked = true;
+    }
+    let myUrl = `https://6180cdd88bfae60017adfc0d.mockapi.io/todos/${taskObj[index].id}`;
+
+    let putTask = await fetch(myUrl, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(taskObj[index]),
+    });
+    if (!putTask.ok) {
+        console.log("We Have an Error");
+    }
+}
+
+
+function showModal(index) {
+    deleteBtn.value = index;
+}
+
+
+async function deleteTask() {
+    let taskObj;
+    spinner.classList.remove("d-none");
+    let deleteIndex = deleteBtn.value;
+    let response = await fetch(url);
+    if (response.ok) {
+        taskObj = await response.json();
+    } else {
+        console.log("We have an Error");
+    }
+    taskObj.reverse();
+
+    let myUrl = `https://6180cdd88bfae60017adfc0d.mockapi.io/todos/${taskObj[deleteIndex].id}`
+    let deleteTask = await fetch(myUrl, {
+        method: "DELETE",
+    });
+    if (!deleteTask.ok) {
+        spinner.classList.add("d-none");
+        console.log("We Have an Error");
+    }else {
+        let queryParams = new URLSearchParams(window.location.search);
+        let newUrl = queryParams.get("page");
+        console.log(newUrl);
+        showTask(newUrl - 1);
+    }
+
+}
+
+function paginationBtn(size = 10) {
+    let btn = '';
+    fetch(url)
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+            btnPaginationList.forEach((elem, i) => {
+                for (let i = 0; i < data.length / size; i++) {
+
+
+                    btn += `<button class='pagination__btn'>${i+1}</button>`
+                }
+                elem.innerHTML = btn;
+            });
+        })
+}
+
+paginationBtn()
+
+
+function addClass(btnElem, prevBtn){
+    prevBtn.forEach(elem => elem.classList.remove('pagination__btn-active'));
+    btnElem.classList.add('pagination__btn-active');
+}
+
+
+showTask(0)
+
